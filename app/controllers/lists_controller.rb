0892125106a593c621021134589195f9d99099e5
Fernanda Freitas Martinez <idea.fernanda@gmail.com>
2022-07-12 19:26:41 +0100

    association between list and movies through bookmark

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -14,6 +14,7 @@ class ListsController < ApplicationController
 
   def show
     @list = List.find(params[:id])
+    @bookmark = Bookmark.new
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :lists do
     resources :bookmarks, only: [:new, :create]
   end
+
+  resources :bookmarks, only: [ :destroy ]
 end
